fe86161e1f305975408eb4c0eec2ec342d9f44be
SnowSword1010 <jmayank2001@gmail.com>
2022-11-18 23:54:23 +0530

    :sparkles: Missing Number

---
+++ Range Queries/static_range_minimum_queries.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define endl "\n"
+using namespace std;
+
+signed main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, q;
+	cin >> n >> q;
+	int A[n];
+    for(int i = 0; i < n; i++){
+        cin >> A[i];
+    }
+    // Minimum prefix array => P[i] denotes minimum 
+    int P[n];
+}
